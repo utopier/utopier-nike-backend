a5bad0ddae7ac63485b8875a7e7cb9f1b434d746
utopier <utopier2025@gmail.com>
2021-06-03 22:23:44 +0900

    Cors Error Tetst

--- src/index.ts
+++ src/index.ts
@@ -32,9 +32,11 @@ const server = new ApolloServer({
 
 const app = express();
 
+app.options('*',cors());
 // app.use(cors({
 //   origin: process.env.NODE_ENV === 'production' ? 'https://utopier.github.io' : true,
-//   credentials: true
+//   credentials: true,
+  
 // }));
 app.use(cors());
 app.use(logger('dev'));
